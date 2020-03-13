fa5c3c23edd4a2cf3f8ac3227a588d984ab64015
Chris Proctor <chris.proctor@gmail.com>
2020-03-13 10:24:43 -0400

    Modal works

---
+++ quest/modal.py
@@ -0,0 +1,77 @@
+# Modal
+# -------------------
+# A modal window is a "pop-up" that creates a mode--it pauses the game until the 
+# window
+
+import arcade
+from quest.modal_button import ModalButton
+from quest.text_label import TextLabelStack
+from textwrap import wrap
+
+class Modal:
+    """ A modal window is a pop-up that pauses the game until it is resolved.
+    It is called a Modal because it puts the game into a new mode. 
+
+    Arguments: 
+        game: The game which the Modal is part of.
+    """
+    width = 400
+    height = 400
+    background_color = arcade.color.WHITE
+
+    def __init__(self, game):
+        self.game = game
+        self.x_center = 0
+        self.y_center = 0
+        self.set_text_labels()
+        self.set_option_labels()
+
+    def update_position(self, x, y):
+        self.x_center = x
+        self.y_center = y
+        self.set_text_labels()
+        self.set_option_labels()
+
+    def set_text_labels(self):
+        self.text_labels = TextLabelStack(
+            ["Sorry to bother you with this modal.", "But it's important."], 
+            self.x_center,
+            self.y_center + self.height / 2
+        )
+
+    def set_option_labels(self):
+        self.option_labels = TextLabelStack(
+            ["OK", "Fine"],
+            self.x_center,
+            self.y_center + 100
+        )
+        self.option_labels.set_highlight(0)
+
+    def choose_option(self, value):
+        """When a button is clicked, it calls choose_option with its value.
+        """
+        self.game.close_modal()
+
+    def on_key_release(self, key, modifiers):
+        pass
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.UP or key == arcade.key.W:
+            i = (self.option_labels.get_highlight() + 1) % len(self.option_labels)
+            self.option_labels.set_highlight(i)
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            i = (self.option_labels.get_highlight() - 1) % len(self.option_labels)
+            self.option_labels.set_highlight(i)
+        elif key == arcade.key.ENTER:
+            self.choose_option(self.option_labels.get_highlight())
+
+    def on_draw(self):
+        arcade.draw_rectangle_filled(
+            self.x_center, 
+            self.y_center, 
+            self.width, 
+            self.height, 
+            self.background_color
+        )
+        self.text_labels.draw()
+        self.option_labels.draw()
---
+++ quest/text_label.py
@@ -0,0 +1,92 @@
+import arcade
+from quest.helpers import tint, shade
+from textwrap import wrap
+
+class TextLabel:
+    """Draws text onto a specific part of the screen. 
+    """
+    width = 400
+    font_size = 16
+    line_height = 20
+    padding = 6
+    font_color = arcade.color.BLACK
+    background_color = arcade.color.LIGHT_GRAY
+    wrap_at = 40
+    highlight = False
+
+    def __init__(self, text, x_center, y_center):
+        self.x_center = x_center
+        self.y_center = y_center
+        if self.wrap_at:
+            self.text_lines = wrap(text, self.wrap_at)
+        else:
+            self.text_lines = [text]
+
+    def draw(self):
+        if self.background_color:
+            arcade.draw_rectangle_filled(
+                self.x_center, 
+                self.y_center, 
+                self.width,
+                self.height(),
+                tint(self.background_color) if self.highlight else self.background_color 
+            )
+        for i, line in enumerate(self.text_lines):
+            arcade.draw_text(
+                line, 
+                self.text_x(),
+                self.text_y(i),
+                tint(self.font_color) if self.highlight else self.font_color,
+                self.font_size
+            )
+
+    def height(self):
+        return len(self.text_lines) * self.line_height + 2 * self.padding
+
+    def text_x(self):
+        return self.x_center - self.width / 2 + self.padding
+    
+    def text_y(self, line=0):
+        y_top = self.y_center + self.height() / 2 
+        return y_top - (line + 1) * (self.padding + self.line_height)
+
+class TextLabelStack:
+    """ Creates a stack of TextLabels
+    """
+    text_label_class = TextLabel
+    margin = 10
+
+    def __init__(self, text_for_labels, x_center, y_top):
+        self.x_center = x_center
+        self.y_top = y_top
+        self.text_labels = []
+        for i, text in enumerate(text_for_labels):
+            label = self.text_label_class(
+                text, 
+                self.x_center,
+                self.y_top - self.height()
+            )
+            label.y_center -= label.height() / 2
+            self.text_labels.append(label)
+
+    def height(self):
+        label_heights = sum([label.height() for label in self.text_labels])
+        margin_heights = (1 + len(self.text_labels)) * self.margin
+        return label_heights + margin_heights
+
+    def draw(self):
+        for label in self.text_labels:
+            label.draw()
+
+    def get_highlight(self):
+        for label_index, label in enumerate(self.text_labels):
+            if label.highlight:
+                return label_index
+
+    def set_highlight(self, i):
+        print("SETTING HIGHLIGHT TO {}".format(i))
+        for label_index, label in enumerate(self.text_labels):
+            label.highlight = (i == label_index)
+
+    def __len__(self):
+        return len(self.text_labels)
