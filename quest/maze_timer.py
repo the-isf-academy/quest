faff913560f29e62d40807a3aa36183b57e2f255
Xuxi <0010109@student.isf.edu.hk>
2020-04-03 15:32:57 +0800

    1. Started the timer feature as Maze extension
    What I changed
    ^^
    
    Why I changed it
    For the group project.
    
    Estimate for remaining time to finish assignment: [unlimited hours]

---
+++ quest/maze_timer.py
@@ -0,0 +1,10 @@
+from quest.examples.maze import MazeGame
+
+class MazeTimer(MazeGame):
+    """An extension of the MazeGame class with a timer in it."""
+
+    def __init__(self):
+        """Initializes the game window and sets up other classes.
+        """
+        super().__init__()
+        self.timer() = 
