f034545e53d4509bd2b07736627057f70a9688d2
Sze <0009090@student.isf.edu.hk>
2020-04-03 15:37:19 +0800

    Currently an empty file, but I've been adding some stuff to a google doc
    What I changed
    I changed and added a base to my feature code, but it has nothing.
    
    Why I changed it
    To start the quest feauture!
    
    Estimate for remaining time to finish assignment: [4 - 7 hours(?)]

---
+++ quest/examples/NPC_game_feature.py
@@ -0,0 +1,11 @@
+class NPC_Maze_Solve(QuestGame):
+""" Lets NPC solve the maze game so you can vs them too!.
+
+Attributes:
+      INIT_ARG_0: description of the first argument in the class's __init__() function
+      INIT_ARG_1: description of the second argument in the class's __init__() function
+"""
+
+if __name__ == '__main__':
+    game = NPC_Maze_Solve()
+    game.run()
