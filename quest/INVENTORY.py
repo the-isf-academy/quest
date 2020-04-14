9aca943216a49f44d3bcf07d05a3198136ed84c7
E.Hazard <0010046@isf.edu.hk>
2020-04-14 11:05:47 +0800

    Update INVENTORY.py
    yeet

--- quest/INVENTORY.py
+++ quest/INVENTORY.py
@@ -2,3 +2,6 @@ def Inventory()
 if player press e and is near item, player picks up the item and then stores it in the player.
 
 every item will have a name and a value, the value is the number of the item that the player owns.
+
+
+Git is stupid
