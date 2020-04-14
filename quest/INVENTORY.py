39a5a5ddcc0cccac4a39f171ac03a23acc93ccb7
Walker Chak <0010007@student.isf.edu.hk>
2020-04-14 10:57:22 +0800

    i did sudo code.

---
+++ quest/INVENTORY.py
@@ -0,0 +1,4 @@
+def Inventory()
+if player press e and is near item, player picks up the item and then stores it in the player.
+
+every item will have a name and a value, the value is the number of the item that the player owns.
