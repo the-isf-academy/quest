24b2534ad76d3aa5d26089722b8a91ba02a16cfc
E.Hazard <0010046@isf.edu.hk>
2020-04-20 09:43:53 +0800

    Update INVENTORY.py
    we translated pseudo-code and added in the on key press function

--- quest/INVENTORY.py
+++ quest/INVENTORY.py
@@ -1,7 +1,120 @@
+
+if player inputs (x) and is near item, player picks up the item and then stores it in the backpack
+when player inputs (e), player opens inventory.
+def on_key_press(self, key, modifiers):
+
+    if self.current_modal:
+        self.current_modal.on_key_press(key, modifiers)
+    else:
+        if key == arcade.key.e:
+            self.display_inventory.
+        if key == arcade.key.x:
+            self.pickup_item.
 def Inventory()
-if player press e and is near item, player picks up the item and then stores it in the player.
+    def store_item():
+        inventory={}
+        if key == arcade.key 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 every item will have a name and a value, the value is the number of the item that the player owns.
-
-
-Git is stupid
