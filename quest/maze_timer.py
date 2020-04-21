3680c276d86c3c13862433a7fae8cff3c6453b94
Xuxi <0010109@student.isf.edu.hk>
2020-04-21 09:58:56 +0800

    In progress for Add-Time function
    What I changed
    We attempted to make a function to add time after completing the countdown function. However, this function does not work currently.
    
    Why I changed it
    For the lab work.
    
    Estimate for remaining time to finish assignment: [a while]

--- quest/maze_timer.py
+++ quest/maze_timer.py
@@ -16,6 +16,13 @@ class MazeTimer(MazeGame):
             t-= 1
             time.sleep(1)
 
+    def add_time(old_t, extra_t):
+        new_t = old_t + extra_t
+#        MazeTimer.countdown(new_t)
+        return new_t
 
 t=5
+MazeTimer.countdown(t)
+#old_t =
+t = MazeTimer.add_time(old_t, 5)
 MazeTimer.countdown(t)
