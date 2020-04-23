5cc66105ffa2304569628507b5abe1dd40a32018
PaulineLi <4616004@student.isf.edu.hk>
2020-04-23 15:29:08 +0800

    still need to work on the code

--- quest/examples/mouse.py
+++ quest/examples/mouse.py
@@ -1,42 +1,22 @@
-from quest.game import QuestGame
-from quest.map import TiledMap
-from quest.sprite import Background, Wall
-import arcade
-import os
-from pathlib import Path
+from quest.examples.island import IslandAdventure
 
-def resolve_path(relative_path):
-    """A helper function to find images and other resources.
-    """
-    here = Path(os.path.abspath(__file__)).parent
-    return str(here / relative_path)
+class MouseMotion(IslandAdventure):
 
-class IslandAdventureMouse(IslandAdventure):
-    """A very simple subclass of :py:class:`QuestGame`.
+    def on_mouse_motion(self, x: float, y: float, dx: float, dy: float):
+        """
+        keep track of the mouse's new coordinates whenever it moves.
 
-    To run this example::
+        :param float x: x position of mouse
+        :param float y: y position of mouse
+        :param float dx: Change in x since the last time this method was called
+        :param float dy: Change in y since the last time this method was called
+        """
 
-        $ python -m quest.examples.island
-
-    :py:class:`IslandAdventure` shows off the basic features of the Quest
-    framework, loading a map and letting the player explore it.
-    After you play it, check out the sorce code by clicking on "source" in the
-    blue bar just above.
-    """
-
-    player_sprite_image = resolve_path("images/boy_simple.png")
-    screen_width = 500
-    screen_height = 500
-    left_viewport_margin = 96
-    right_viewport_margin = 96
-    bottom_viewport_margin = 96
-    top_viewport_margin = 96
-    player_initial_x = 300
-    player_initial_y = 300
-    player_speed = 6
-
-    def on_mouse_motion
+        if self.player.center_y != y:
+            self.player.change_y = dy
+        if self.player.center_x != x:
+            self.player.change_x = dx
 
 if __name__ == '__main__':
-    game = IslandAdventureMouse()
+    game = MouseMotion()
     game.run()
