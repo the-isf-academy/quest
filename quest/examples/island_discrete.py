e1085d1fd9aad04907446c4479b969134f80fff3
Chris Proctor <github.com@accounts.chrisproctor.net>
2022-05-18 08:26:41 -0400

    Update island discrete example

--- quest/examples/island_discrete.py
+++ quest/examples/island_discrete.py
@@ -17,12 +17,12 @@ class IslandAdventureDiscrete(QuestGame):
     player_sprite_image = resolve_resource_path("images/boy_simple.png")
     player_initial_x = 300
     player_initial_y = 300
-    player_speed = 6
+    player_speed = 8
 
     def setup_maps(self):
         """Sets up the map.
 
-        Uses a :py:class:`TiledMap` to load the map from a ``.tmx`` file,
+        Uses a :py:class:`TiledMap` to load the map from a ``.json`` file,
         created using :doc:`Tiled <tiled:manual/introduction>`.
         """
         super().setup_maps()
@@ -30,7 +30,7 @@ class IslandAdventureDiscrete(QuestGame):
             "Obstacles": Wall,
             "Background": Background,
         }
-        self.add_map(TiledMap(resolve_resource_path("images/island/island.tmx"), sprite_classes))
+        self.add_map(TiledMap(resolve_resource_path("images/island/island.json"), sprite_classes))
         layer = GridMapLayer("grid", 40, 40, 40*32, 40*32)
         self.get_current_map().add_layer(layer)
 
