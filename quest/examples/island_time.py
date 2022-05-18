5b03b0aaf437cc3272d329721281891affea9f11
Chris Proctor <github.com@accounts.chrisproctor.net>
2022-05-18 10:55:46 -0400

    Refactor, clean island time example

--- quest/examples/island_time.py
+++ quest/examples/island_time.py
@@ -9,6 +9,8 @@ from quest.contrib.timer import TimerMixin
 import arcade
 import os
 from pathlib import Path
+from math import cos, pi, floor
+from quest.helpers import shade
 
 class TimedContinuousPhysicsEngine(ContinuousPhysicsEngine, TimerMixin):
     """ Physics engine that also makes time-based updates
@@ -23,12 +25,45 @@ class TimedContinuousPhysicsEngine(ContinuousPhysicsEngine, TimerMixin):
         of the current day/night cycle.
         """
         super().update()
-        time_since_start = self.time_since_start() 
-        curr_mod = time_since_start%self.game.time_cycle_secs
-        grade = abs(curr_mod - self.game.time_cycle_secs/2) / (self.game.time_cycle_secs/2)
-        color_value = grade*(255-self.game.max_darkness) + self.game.max_darkness
+        white = (255,255,255)
+        shade_ratio = self.game.max_shade * (cos(self.time_of_day() * 2 * pi) + 1) / 2
+        brightness = shade(white, ratio=shade_ratio)
         for sprite in self.all_sprites:
-            sprite.color = (color_value, color_value, color_value)
+            sprite.color = brightness
+
+    def time_of_day(self):
+        "Returns the time of day as a ratio from 0 to 1."
+        return (self.time_since_start() % self.game.day_length) / self.game.day_length
+
+    def clock_time(self):
+        "Returns an hour:minute string"
+        tod = self.time_of_day()
+        hour = floor(tod * 24)
+        hour_fraction = (tod - hour/24) * 24
+        minute = floor(hour_fraction * 60)
+        ampm = "am" if hour < 12 else "pm"
+        ampm_hour = hour % 12 if (hour % 12) > 0 else 12
+        return f"{ampm_hour}:{minute:02d} {ampm}"
+
+    def time_of_day_string(self):
+        "Returns the time of day as a string"
+        hour = self.time_of_day() * 24
+        if hour < 5:
+            return "night"
+        elif hour < 7:
+            return "twilight"
+        elif hour < 9:
+            return "dawn"
+        elif hour < 12:
+            return "morning"
+        elif hour < 17:
+            return "afternoon"
+        elif hour < 20:
+            return "evening"
+        elif hour < 22:
+            return "dusk"
+        else:
+            return "night"
 
 
 class IslandTime(IslandAdventure):
@@ -36,8 +71,8 @@ class IslandTime(IslandAdventure):
     and shading the sprites based on a day/night cycle
     """
 
-    max_darkness = 100
-    time_cycle_secs = 30
+    max_shade = 0.6
+    day_length = 20
     display_time = True
     
     def __init__(self):
@@ -55,7 +90,9 @@ class IslandTime(IslandAdventure):
         """ Displays the time in the game relative to the time cycle
         """
         if self.display_time:
-            return "Time: {}".format(int(self.physics_engine.time_since_start()%self.time_cycle_secs))
+            tods = self.physics_engine.time_of_day_string().capitalize()
+            clock_time = self.physics_engine.clock_time()
+            return f"{tods} ({clock_time})"
 
 if __name__ == '__main__':
     game = IslandTime()
